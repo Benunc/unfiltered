b09ec958c00f0f4b748f29d7c57503b89590a586
Brian Gardner <bmg1227@gmail.com>
2014-03-10 20:15:14 -0500

    Derp.

--- functions.php
+++ functions.php
@@ -194,12 +194,10 @@ function unfiltered_custom_gravatar ($avatar) {
 remove_action( 'genesis_footer', 'genesis_footer_markup_open', 5 );
 remove_action( 'genesis_footer', 'genesis_do_footer' );
 remove_action( 'genesis_footer', 'genesis_footer_markup_close', 15 );
-add_action( 'genesis_footer', 'bg_footer' );
-	function bg_footer() { ?>
+add_action( 'genesis_footer', 'unfiltered_custom_footer' );
+	function unfiltered_custom_footer() { ?>
 
-	<div class="site-footer"><div class="wrap"><p>Powered by <a href="http://www.starbucks.com/">Starbucks lattes</a>, <a href="http://www.sarahmclachlan.com/">really good music</a> and the <a href="http://www.studiopress.com/">Genesis Framework</a>.</p><p>Follow me on <a href="http://www.facebook.com/bgardner">Facebook</a>, <a href="http://plus.google.com/109450535379570250650?rel=author">Google+</a>, <a href="http://instagram.com/bgardner">Instagram</a>, <a href="http://twitter.com/bgardner" rel="me">Twitter</a>. <a href="http://unfiltered.me/uncopyright/">Uncopyright</a>.</p>
-	
-	</div></div>
+	<div class="site-footer"><div class="wrap"><p>Powered by <a href="http://www.starbucks.com/">Starbucks lattes</a>, <a href="http://www.sarahmclachlan.com/">really good music</a> and the <a href="http://www.studiopress.com/">Genesis Framework</a>.</p><p>Follow me on <a href="http://www.facebook.com/bgardner">Facebook</a>, <a href="http://plus.google.com/109450535379570250650?rel=author">Google+</a>, <a href="http://instagram.com/bgardner">Instagram</a>, <a href="http://twitter.com/bgardner" rel="me">Twitter</a>. <a href="http://unfiltered.me/uncopyright/">Uncopyright</a>.</p></div></div>
 
 	<?php
 }
